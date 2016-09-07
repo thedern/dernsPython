128ad8f20f57a317e45d80e9ad7d11209dade5c6
U-CORP\darren.smith <darren.smith@5CG529439G.corp.advancestores.com>
2016-09-07 10:24:42 -0400

    adding b2b_ripplestart_final.py

---
+++ b2b_ripplestart_final.py
@@ -0,0 +1,35 @@
+
+#Author:  Darren Smith
+#Date:  04/10/2012
+#Purpose:  Restarts the B2B Production cluster via the ripplestart command.  Must be executed as wasuser2.
+
+import sys
+
+#-----------------------------------
+
+def logger(x):
+	logfile = open('/backup/ecomm/sysadm/jython/b2b_ripplestart.log', 'a')
+	logfile.write(x)
+	logfile.close()
+
+#-----------------------------------
+
+#timestamp
+import time
+from time import strftime
+now = strftime("%Y-%m-%d %H:%M:%S")
+formatNow = now + "\n"
+logger(formatNow)
+
+startMsg = "B2B Ripplestart Initiated" + "\n"
+errorMsg = "B2B Ripplestart Failed" + "\n" + "\n"
+completeMsg = "B2B Ripplestart Completed" + "\n" + "\n"
+
+
+logger(startMsg)
+
+try:
+	AdminControl.invoke('WebSphere:name=WC_cve_cluster,process=dmgr,platform=common,node=prd1CellManager01,version=7.0.0.13,type=Cluster,mbeanIdentifier=WC_cve_cluster,cell=prd1Cell01,spec=1.0', 'rippleStart')
+	logger(completeMsg)
+except RuntimeError:
+        logger(errorMsg)
