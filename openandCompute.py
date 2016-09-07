e2b318e106e051eab7da5da4eb4c3a1b8c6e5138
thedern <dxsmith12@soverin.net>
2016-09-06 22:23:19 -0400

    adding openandCompute.py

---
+++ openandCompute.py
@@ -0,0 +1,46 @@
+
+#Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+#X-DSPAM-Confidence:    0.8475
+#Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below.
+#You can download the sample data at http://www.pythonlearn.com/code/mbox-short.txt when you are testing below enter mbox-short.txt as the file name.
+
+#Note str.capitalize() only caps the first letter in the string.  str.upper() caps the whole string.
+
+
+while True:
+        try:
+                test = raw_input('please enter file name ')
+                fhand = open(test)
+                break
+
+        except:
+                print "the file named '%s' does not exist" % test
+                continue
+
+counter = 0
+totnum = 0
+
+for line in fhand:
+        line = line.rstrip()   #strip the new line or white space off the end
+
+        x = line.find('X-DSPAM-Confidence')  #use string's find method to look for the substring in quotes
+
+        if x == 0:  #if return code is '0', meaning a match perform the next commands
+
+                y = line.find(' ') #find the space in the substring
+                num = line[y+1:]   #slice from 1 character after the space to the end of the string and save
+                fnum = float(num)  #convert to float
+                #print "fnum is ", fnum
+                totnum = totnum + fnum  #keep running total of sums
+                #print "totmun + fnum is ", totnum
+                counter = counter + 1 #keep running counter
+
+
+print "total count is: ", counter
+print "total number is: ", totnum
+
+ave = totnum / counter
+print "Average spam confidence:", ave  #take average
+
+
+
