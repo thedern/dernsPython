4b258566c4dae3063a72966d144f8e3b24409d31
thedern <dxsmith12@soverin.net>
2016-09-07 10:28:29 -0400

    adding everyOther.py

---
+++ everyOther.py
@@ -0,0 +1,20 @@
+
+"""
+Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+string_bits('Hello') 'Hlo'
+string_bits('Hi') 'H'
+string_bits('Heeololeo') 'Hello'
+"""
+
+def string_bits(str):
+    n = len(str)
+    x = 0
+    new = ""
+    while x < n:
+	print str[x]
+	x += 2
+        print str[x]
+	
+string_bits('Hello')
+	
