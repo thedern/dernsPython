27e78de70e4ea0188acbce8eaa5997eb290302d7
thedern <dxsmith12@soverin.net>
2016-09-23 18:06:59 -0400

    Added Try/Except block

--- hostlisttoIP.py
+++ hostlisttoIP.py
@@ -1,52 +1,54 @@
 # Takes a list of hosts via text file and produces the IP addresses in a new
 # file.
+try:
+    import socket
+    from sys import argv
 
-import socket
-from sys import argv
+    script, hostlistFile, hostIPlistFile = argv
 
-script, hostlistFile, hostIPlistFile = argv
+    if len(argv) == 0:
+        # Print useage
+        print "useage is scriptname inputfile outputfile"
+        exit()
+    else:
+        pass
 
-if len(argv) == 0:
-    # Print useage
-    print "useage is scriptname inputfile outputfile"
-    exit()
-else:
-    pass
+    infile = open(hostlistFile)
+    outfile = open(hostIPlistFile, 'w')
+    # truncate to nsure we are creating a fresh file
+    outfile.truncate()
 
-infile = open(hostlistFile)
-outfile = open(hostIPlistFile, 'w')
-# truncate to nsure we are creating a fresh file
-outfile.truncate()
+    # create global dictionary
+    hostIP = dict()
 
-# create global dictionary
-hostIP = dict()
+    def getIP(hostname):
+        global hostIP
+        try:
+            ipaddr = socket.gethostbyname(hostname)
+            # update global dictionary
+            hostIP[hostname] = [ipaddr]
+        except:
+            print "there is no host ", hostname
 
+    for line in infile:
+        line = line.rstrip()
+        # creates list in the event the infile line has more than hostname
+        word = line.split()
+        # skips blank lines and passed hostame to getIP
+        if len(word) > 0:
+            # pass hostname to getIP function
+            getIP(word[0])
 
-def getIP(hostname):
-    global hostIP
-    try:
-        ipaddr = socket.gethostbyname(hostname)
-        # update global dictionary
-        hostIP[hostname] = [ipaddr]
-    except:
-        # clause for typos or non-existant hosts
-        print "there is no host ", hostname
+    # write completed dictionary to outfile
+    # obtain keys from dictionary
+    keez = hostIP.keys()
 
-for line in infile:
-    line = line.rstrip()
-    # creates list in the event the infile line has more than hostname
-    word = line.split()
-    # skips blank lines and passed hostame to getIP
-    if len(word) > 0:
-        # pass hostname to getIP function
-        getIP(word[0])
-
-# write completed dictionary to outfile
-# obtain keys from dictionary
-keez = hostIP.keys()
-
-# iterate through keys list and print a formatted key/value pair
-for i in keez:
-    z = "\n %s \t %s" % (i, hostIP[i])
-    print z
-    outfile.write(z)
+    # iterate through keys list and print a formatted key/value pair
+    for i in keez:
+        z = "\n %s \t %s" % (i, hostIP[i])
+        print z
+        outfile.write(z)
+except:
+    if len(argv) != 3:
+        print """you messed up, usage is <scriptname>
+                  <infile name> <outfile> name"""
