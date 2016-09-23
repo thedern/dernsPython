6c736bb908e25fa0789ab6b14c46e097feaafb77
thedern <dxsmith12@soverin.net>
2016-09-23 14:51:23 -0400

    Adding host to IP checker

---
+++ hostlisttoIP.py
@@ -0,0 +1,40 @@
+# Takes a list of hosts via text file and produces the IP addresses
+import socket
+
+infile = open('hostList.txt')
+outfile = open('hostIPMap.txt', 'w')
+# truncate to nsure we are creating a fresh file
+outfile.truncate()
+
+# create global dictionary
+hostIP = dict()
+
+
+def getIP(hostname):
+    global hostIP
+    try:
+        ipaddr = socket.gethostbyname(hostname)
+        # update global dictionary
+        hostIP[hostname] = [ipaddr]
+    except:
+        # clause for typos or non-existant hosts
+        print "there is no host ", hostname
+
+for line in infile:
+    line = line.rstrip()
+    # creates list in the event the infile line has more than hostname
+    word = line.split()
+    # skips blank lines and passed hostame to getIP
+    if len(word) > 0:
+        # pass hostname to getIP function
+        getIP(word[0])
+
+# write completed dictionary to outfile
+# obtain keys from dictionary
+keez = hostIP.keys()
+
+# iterate through keys list and print a formatted key/value pair
+for i in keez:
+    z = "\n %s \t %s" % (i, hostIP[i])
+    print z
+    outfile.write(z)
