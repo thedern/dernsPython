b5f09348502bd54c10f57b318b6eb0b5c0a5da3a
thedern <dxsmith12@soverin.net>
2016-11-01 17:17:35 -0400

    Formatting Changes

--- stringSlice.py
+++ stringSlice.py
@@ -3,7 +3,7 @@
 # the number at the end of the line below. Convert the extra#cted value to
 # a floating point number and print it out.
 
-##text = "X-DSPAM-Confidence:    0.8475";
+# text = "X-DSPAM-Confidence:    0.8475";
 
 str = 'X-DSPAM-Confidence: 0.8475'
 
