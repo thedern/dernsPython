3851d383a10d6b5fb266876b654509e0db5ff75f
thedern <dxsmith12@soverin.net>
2016-11-01 17:37:41 -0400

    mass update

--- splitandList2.py
+++ splitandList2.py
@@ -1,8 +1,12 @@
 
 
-# Open the file mbox-short.txt and read it line by line. When you find a line that starts with 'From ' like the following line:
+# Open the file mbox-short.txt and read it line by line. When you find a line
+# that starts with 'From ' like the following line:
 # From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
-# You will parse the From line using split() and print out the second word in the line (i.e. the entire address of the person who sent the message). Then print out a count at the end.
+# You will parse the From line using split() and print out the second
+# word in the line (i.e. the entire address of the person
+# who sent the message).
+# Then print out a count at the end.
 # Hint: make sure not to include the lines that start with 'From:'.
 
 # You can download the sample data at
@@ -15,7 +19,7 @@ if len(fname) < 1:
 fhandle = open(fname)
 
 
-fhandle = open('mbox-short.txt')
+# fhandle = open('./mbox-short.txt')
 count = 0
 
 for line in fhandle:
